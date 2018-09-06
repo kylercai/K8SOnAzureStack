22fdc5f3f0540327e4e8c1a7519bda3bdbea199d
Kai Cai <kcai.nortel@gmail.com>
2018-09-06 20:01:01 +0800

    init version 2
    s Please enter the commit message for your changes. Lines starting

--- AzureStack.AcsEngine.psm1
+++ AzureStack.AcsEngine.psm1
@@ -66,162 +66,6 @@ function Set-AcseAzureStackEnvironment
 	Write-Verbose "Tenant environment created at endpoint: $ArmEndpoint"
 }
 
-function New-AcseServicePrincipal
-{
-    param
-    (
-		[Parameter(Mandatory = $true)]
-		[string]$AadTenantId,
-
-        [Parameter(Mandatory = $true)]
-		[PSCredential]$ServiceAdminCredential
-    )
-   
-    $session = New-PSSession -ErrorAction Stop
-
-    Invoke-Command -Session $session -ScriptBlock {
-
-        # If we are logged in we want to ensure that we logout.
-        $null = Logout-AzureRmAccount -ErrorAction Ignore | Out-Null
-
-        Write-Verbose "Logging to AzureCloud with tenantId: $using:AadTenantId" -Verbose
-        $publicCloud = Get-AzureRmEnvironment -Name AzureCloud
-        $null = Login-AzureRmAccount -Environment $publicCloud -Credential $using:ServiceAdminCredential -TenantId $using:AadTenantId -ErrorAction Stop
-
-        $applicationName = "Kubernetes-WLK-$(New-Guid)"
-        $password = (New-Guid).ToString()
-        $application = New-AzureRmADApplication -DisplayName $applicationName -IdentifierUris "http://$applicationName" -HomePage "http://localhost" -Password $password   
-        $applicationId = $application.ApplicationId
-        $null = New-AzureRmADServicePrincipal -ApplicationId $applicationId
-       
-        $result = @{'applicationId'=$applicationId;'password'=$password}
-        $result
-    }  
-    
-    Remove-PSSession -Session $session
-}
-
-function Assign-AcseServicePrincipal
-{
-    param
-    (
-		[Parameter(Mandatory = $true)]
-		[string]$TenantArmEndpoint,
-
-		[Parameter(Mandatory = $true)]
-		[string]$AadTenantId,
-
-		[Parameter(Mandatory = $true)]
-		[PSCredential]$TenantAdminCredential,
-
-		[Parameter(Mandatory = $true)]
-		[string]$TenantSubscriptionId,
-		
-		[Parameter(Mandatory = $true)]
-		[string]$ApplicationId
-    )
-   
-    $session = New-PSSession -ErrorAction Stop
-
-    Invoke-Command -Session $session -ScriptBlock {
-
-		$environmentName = "AzureStackUser"
-        $environment = Get-AzureRmEnvironment -Name $environmentName
-		if ($environment -eq $null)
-		{
-			$environment = Add-AzureRmEnvironment -Name $environmentName -ArmEndpoint $using:TenantArmEndpoint -Verbose -ErrorAction Stop
-		}
-		Login-AzureRmAccount -EnvironmentName $environmentName -TenantId $using:AadTenantId -Credential $using:TenantAdminCredential -ErrorAction Stop | Out-Null
-        
-		$subscription = Select-AzureRmSubscription -SubscriptionId $using:TenantSubscriptionId
-
-		Write-Verbose "Assigning SPN: $using:ApplicationId to subcription: $using:TenantSubscriptionId." -Verbose
-		New-AzureRmRoleAssignment -RoleDefinitionName Contributor -ServicePrincipalName $using:ApplicationId | Out-Null
-    }  
-    
-    Remove-PSSession -Session $session
-}
-
-function New-AcseStorageAccount
-{
-    param
-    (
-		[Parameter(Mandatory = $true)]
-        [String] $AadTenantId,
-
-		[Parameter(Mandatory = $true)]
-        [String] $Location,
-
-        [Parameter(Mandatory = $true)]
-        [PSCredential] $TenantAdminCredential,
-
-        [Parameter(Mandatory = $true)]
-        [string] $TenantArmEndpoint,
-
-		[Parameter(Mandatory = $true)]
-        [string] $TenantSubscriptionId,
-
-		[Parameter(Mandatory = $true)]
-        [string] $LocalFilePath,
-
-		[Parameter(Mandatory = $false)]
-        [string] $NamingSuffix
-    )
-
-	$environmentName = "AzureStackUser"
-	$environment = Get-AzureRmEnvironment -Name $environmentName
-    if ($environment -eq $null)
-    {
-        $environment = Add-AzureRmEnvironment -Name $environmentName -ArmEndpoint $TenantArmEndpoint -Verbose -ErrorAction Stop
-    }
-	Login-AzureRmAccount -EnvironmentName $environmentName -TenantId $AadTenantId -Credential $TenantAdminCredential  -ErrorAction Stop | Out-Null
-
-	$subscription = Select-AzureRmSubscription -SubscriptionId $TenantSubscriptionId -ErrorAction Stop
-
-	if (-not ($NamingSuffix)) 
-	{
-		$NamingSuffix = 10000..99999 | Get-Random
-	}
-	$resourceGroupName = "k8ssa-" + $NamingSuffix
-	$storageAccountName = "k8ssa" + $NamingSuffix
-	$containerName = "k8ssaci" + $NamingSuffix
-
-	#Write-Verbose "Creating or retrieving resource group: $resourceGroupName." -Verbose
-	#if (-not (Get-AzureRmResourceGroup -Name $resourceGroupName -Location $Location -ErrorAction SilentlyContinue)) 
-	#{
-	#	New-AzureRmResourceGroup -Name $resourceGroupName -Location $Location -ErrorAction Stop| Out-Null
-	#}
-
-	#Write-Verbose "Creating or retrieving storage account: $storageAccountName." -Verbose
-	$storageAccount = Get-AzureRmStorageAccount -Name $storageAccountName -ResourceGroupName $resourceGroupName -ErrorAction SilentlyContinue
-	#if (-not ($storageAccount)) 
-	#{
-		#$storageAccount = New-AzureRmStorageAccount -AccountName $storageAccountName -Location $Location -ResourceGroupName $resourceGroupName -Type Standard_LRS -ErrorAction Stop
-	#	$storageAccount = New-AzureRmStorageAccount -AccountName $storageAccountName -Location $Location -ResourceGroupName $resourceGroupName -SkuName Standard_LRS
-	#}
-	Write-Verbose "Storage account AbsoluteUri: $($storageAccount.PrimaryEndpoints.Blob.AbsoluteUri)"
-	Set-AzureRmCurrentStorageAccount -StorageAccountName $storageAccountName -ResourceGroupName $resourceGroupName | Out-Null
-    
-	#Write-Verbose "Creating or retrieving container account: $containerName." -Verbose
-	$container = Get-AzureStorageContainer -Name $containerName -ErrorAction SilentlyContinue
-	#if (-not ($container)) 
-	#{
-	#	$container = New-AzureStorageContainer -Name $containerName -Permission Blob
-	#}
-
-	$fileName = Split-Path $LocalFilePath -Leaf
-	Set-AzureStorageBlobContent -File $LocalFilePath -Container $containerName -Blob $fileName -Force | Out-Null
-
-	[string]$apiModelBlobPath = '{0}{1}/{2}' -f $storageAccount.PrimaryEndpoints.Blob.AbsoluteUri, $containerName, $fileName
-	Write-Verbose "Uploaded the API model to: $apiModelBlobPath." -Verbose
-
-	[string]$blobRootPath = '{0}{1}' -f $storageAccount.PrimaryEndpoints.Blob.AbsoluteUri, $containerName
-	Write-Verbose "Blob root path: $blobRootPath." -Verbose
-
-	$result = @{'apiModelBlobPath'=$apiModelBlobPath;'blobRootPath'=$blobRootPath;'storageAccountResourceGroup'=$resourceGroupName;'storageAccountName'=$storageAccountName;}
-    $result
-}
-
 function Prepare-AcseApiModel
 {
     param
@@ -251,10 +95,10 @@ function Prepare-AcseApiModel
 		[string]$NamingSuffix,
 
 		[Parameter(Mandatory = $false)]
-		[string]$HyperCubeImage = "msazurestackdocker/kubernetes:20180321.1.1.7.14",
+		[string]$HyperCubeImage = "msazurestackdocker/k8s1.9:latest",
 
 		[Parameter(Mandatory = $false)]
-		[string]$HyperCubeImageVersion = "1.7"
+		[string]$HyperCubeImageVersion = "1.9"
     )
 
     # Retrieve Stamp information.
@@ -287,10 +131,6 @@ function Prepare-AcseApiModel
 	Write-Verbose "Adding Tenant AzureStack Environment." -Verbose
     $environment = Set-AcseAzureStackEnvironment -ArmEndpoint $tenantArmEndpoint -EnvironmentName "AzureStackUser"
 
-    # Create service principal in AAD
-    $spn = New-AcseServicePrincipal -AadTenantId $aadTenantId -ServiceAdminCredential $ServiceAdminCredential
-    Write-Verbose "Created new SPN ClientID: $($spn.applicationId), Secret: $($spn.password)" -Verbose
-
     # Prepare the API model based on the current AzureStack environment.
 	Write-Verbose "Preparing the API model" -Verbose
     $apiModel = ConvertFrom-Json (Get-Content -Path  "$PSScriptRoot\azurestack-default.json" -Raw -ErrorAction Stop)
@@ -311,6 +151,7 @@ function Prepare-AcseApiModel
 
 	$apiModel.properties.linuxProfile.ssh.publicKeys[0].keyData = $LinuxVmSshKey
 
+	####pre-set the spn.applicationId, spn.password, or modify the value in the generated apiModel file
     $apiModel.properties.servicePrincipalProfile.clientId = $spn.applicationId
     $apiModel.properties.servicePrincipalProfile.secret = $spn.password
 
@@ -318,18 +159,6 @@ function Prepare-AcseApiModel
 	$localFilePathForApiModel = "$PSScriptRoot\azurestack.json"
     Write-Verbose "azurestack.json: $($PSScriptRoot)" -Verbose
     $apiModel | ConvertTo-Json -Depth 100 | Out-File -FilePath $localFilePathForApiModel -Encoding ascii
-
-	$saParameters = @{'AadTenantId' = $aadTenantId;
-					'Location' = $regionName;
-					'TenantAdminCredential' = $TenantAdminCredential;
-					'TenantArmEndpoint' = $tenantArmEndpoint;
-					'TenantSubscriptionId' = $TenantSubscriptionId;
-					'LocalFilePath' = $localFilePathForApiModel;
-					'NamingSuffix' = $NamingSuffix }
-
-	Write-Verbose "Upload the locally created API model to a Storage Account." -Verbose
-	$apiModel = New-AcseStorageAccount @saParameters
-	$apiModel.Add('spnApplicationId', $spn.applicationId);
 
 	$apiModel
 }
